0c39dd23906fc0ab99d72191eb3f10db291cc87e
AllenExtractor <dayarmustfa@gmail.com>
2026-06-09 14:03:26 +0530

    𝐓𝐞𝐚𝐦 𝐂𝐢𝐧𝐝𝐞𝐫𝐞𝐥𝐥𝐚

--- config.py
+++ config.py
@@ -1,24 +1,24 @@
 import os
 from os import getenv
 # ---------------R---------------------------------
-API_ID = int(os.environ.get("API_ID", "20369373"))
+API_ID = int(os.environ.get("API_ID", "33853339"))
 # ------------------------------------------------
-API_HASH = os.environ.get("API_HASH", "0d53cc7f978163fed3263be5cbb20ab0")
+API_HASH = os.environ.get("API_HASH", "d44e3a158d9da849df318173268f94c0")
 # ----------------D--------------------------------
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 # -----------------A-------------------------------
-BOT_USERNAME = os.environ.get("Masterextratorraonebot")
+BOT_USERNAME = os.environ.get("FuryExtractor_Bot")
 # ------------------X------------------------------
-OWNER_ID = int(os.environ.get("OWNER_ID", "5680454765"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "8909902924"))
 # ------------------X------------------------------
-CREATOR_ID = int(os.environ.get("CREATOR_ID", "5680454765"))
-LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID", "-1002585176866"))
+CREATOR_ID = int(os.environ.get("CREATOR_ID", "8909902924"))
+LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID", "-1003597599758"))
 
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5680454765").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
 # ------------------------------------------------
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002585176866"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003780210843"))
 # ------------------------------------------------
-MONGO_URL = os.environ.get("MONGO_URL", "")
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://developerbro723_db_user:9axC7c7iQm0G3ESO@cluster0.dr8m75m.mongodb.net/?appName=Cluster0")
 # -----------------------------------------------
-PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002585176866"))
+PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1004291233454"))
